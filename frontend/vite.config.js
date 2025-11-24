7c39e05d6fe7e0e61a7cf3b8ee487725a1fd9e8d
Isac Ojaniemi <39057456+IsaOja@users.noreply.github.com>
2025-11-24 08:57:29 +0100

    fixade lite mer för bundling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,18 @@ export default defineConfig({
       "/api": "http://localhost:3000",
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 });
